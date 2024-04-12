ba0fe2d269e5a50991c6fdc51344a104afbf5541
blacktache <adnanemandili@gmail.com>
2024-04-12 05:34:49 +0000

    base url

--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const baseurl = process.env.NEXTAUTH_URL || "http://localhost:3000";
+const baseurl = process.env.NEXTAUTH_URL;
 const redirectUrl = `${baseurl}/register`;
 
 // const handleClick = signIn("github");
